25e02039ed8cdc3e55a8a9b192c816e479796571
CaioMP <Caiodemoura12@gmail.com>
2018-09-01 12:38:22 -0300

    + Animação para mostrar e esconder os áudios da playlist

--- static/channel/js/playlist-play.js
+++ static/channel/js/playlist-play.js
@@ -1,17 +1,26 @@
 function showAudiosPl() {
-    document.getElementById('infoAudio').style.display = "none";
-    document.getElementById('footer').style.display = "none";
+    document.getElementById('infoAudio').style.transform = "translateX(110%)";
+    document.getElementById('footer').style.transform = "translateX(110%)";
     document.getElementById('hideHeader').style.display = "none";
     document.getElementById('showAudiosPl').style.display = "none";
-    document.getElementById('header').style.overflow = "auto";
-    document.getElementById('playlistAudios').style.display = "block";
+    document.getElementById('header').style.maxHeight = "100%";
+    document.getElementById('playlistAudios').style.marginTop = "0";
+    document.getElementById('playlistAudios').style.transform = "translateX(0)";
+    document.getElementById('infoAudio').style.display = "none";
+    setTimeout(function(){
+        document.getElementById('header').style.overflow = "auto";
+    }, 500);
 }
 
 function hideAudiosPl() {
-    document.getElementById('infoAudio').style.display = "block";
-    document.getElementById('footer').style.display = "block";
+    document.getElementById('infoAudio').style.transform = "translateX(0)";
+    document.getElementById('footer').style.transform = "translateX(0)";
     document.getElementById('hideHeader').style.display = "block";
     document.getElementById('showAudiosPl').style.display = "block";
     document.getElementById('header').style.overflow = "hidden";
-    document.getElementById('playlistAudios').style.display = "none";
+    document.getElementById('header').style.maxHeight = "4.3rem";
+    document.getElementById('playlistAudios').style.marginTop = "-3rem";
+    document.getElementById('playlistAudios').style.transform = "translateX(-110%)";
+    document.getElementById('infoAudio').style.display = "block";
+
 }
