1256bef268432537503705d119b4416618aeaafb
CaioMP <Caiodemoura12@gmail.com>
2018-09-15 20:32:30 -0300

    + Design de histórico

--- account/urls.py
+++ account/urls.py
@@ -10,10 +10,8 @@ urlpatterns = [
     path('logout/', login_required(views.logout_view), name='sign_out'),
     path('signup/', views.RegisterView, name='sign_up'),
     path('profile/edit/', login_required(views.edit_profile), name='edit_profile'),
-    path("following/<int:cod>/", views.following, name='following'),
-    path("historic/<int:cod>/", views.historic, name='historic'),
+    path("historic/", views.historic, name='historic'),
     path("new_channel/", login_required(views.NewChannelView), name='new_channel'),
-    path("myPlaylist/<int:cod>/", views.myPlaylists, name='myplaylists'),
     path("change_password/", login_required(views.change_password), name='change_password')
 
 ]
