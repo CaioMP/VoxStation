85238eac7d30dae48dc96e50f6454c06b8a5ab79
CaioMP <Caiodemoura12@gmail.com>
2018-09-27 13:39:11 -0300

    Navbar esconde ao descer a tela

--- static/base.js
+++ static/base.js
@@ -8,6 +8,46 @@ $(document).ready(function () {
     $('[rel="tooltip').on('click', function () {
         $(this).tooltip('hide');
     });
+
+    var prevScrollpos = window.pageYOffset;
+
+    window.onscroll = function() {
+        var currentScrollPos = window.pageYOffset;
+        console.log("prevScrollpos",prevScrollpos,"currentScrollPos",currentScrollPos);
+        if (prevScrollpos > currentScrollPos) {
+            document.getElementById("navbar").style.transform = "translateY(0)";
+        } else {
+            if(window.innerWidth > 575){
+                if(currentScrollPos > 70){
+                    document.getElementById("navbar").style.transform = "translateY(-104%)";
+                    if($("#sidebar").hasClass("active")==false){
+                        $("#sidebar").addClass("active");
+                        $("#sidebarCollapse").addClass("active");
+                    }
+                }
+            }
+            if(window.innerWidth < 576){
+                if(currentScrollPos > 140){
+                    document.getElementById("navbar").style.transform = "translateY(-104%)";
+                    if($("#sidebar").hasClass("active")==false){
+                        $("#sidebar").addClass("active");
+                        $("#sidebarCollapse").addClass("active");
+                    }
+                }
+            }
+            if(window.innerWidth < 415){
+                if(currentScrollPos > 110){
+                    document.getElementById("navbar").style.transform = "translateY(-104%)";
+                    if($("#sidebar").hasClass("active")==false){
+                        $("#sidebar").addClass("active");
+                        $("#sidebarCollapse").addClass("active");
+                    }
+                }
+            }
+
+        }
+        prevScrollpos = currentScrollPos;
+    }
 });
 
 $(function () {
